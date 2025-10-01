34bc928c632ebcab481f378880497bcf2a054d82
campaign <dion@robot-nano.tail2751a7.ts.net>
2025-10-01 13:35:08 +0700

    Get real robot working

--- launch/my_launch_robot.launch.py
+++ launch/my_launch_robot.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','my_rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
 
@@ -34,7 +34,7 @@ def generate_launch_description():
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description}, 
+        parameters=[{'robot_description': robot_description},
                       controller_params_file]
     )
 
@@ -42,7 +42,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -55,7 +55,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
@@ -87,10 +87,6 @@ def generate_launch_description():
     return LaunchDescription([
         ## original
         rsp,
-        #joystick,
-        #twist_mux,
-        # diff_drive_spawner,
-        # joint_broad_spawner
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
